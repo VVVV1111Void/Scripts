f2e61d618b49ae986c7360745b064dd9f0a2cd5e
VVVV1111Void <95571792+VVVV1111Void@users.noreply.github.com>
2022-04-29 15:51:04 +0800

    commit 2

---
+++ Stuff.gd
@@ -0,0 +1,12 @@
+extends Object
+class Body:
+	var body_name : int
+	var body_type : int
+	
+	
+
+
+class System:
+	var star_name : String
+	var star_type : int  # Which tile to use
+	var planets : Dictionary
